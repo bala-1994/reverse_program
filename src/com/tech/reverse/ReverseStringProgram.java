ece880905001e94ba2687449c981bbcb15bd87c4
bala-1994 <balaiahyadav1994@gmail.com>
2020-04-12 16:56:04 +0530

    added logic

--- src/com/tech/reverse/ReverseStringProgram.java
+++ src/com/tech/reverse/ReverseStringProgram.java
@@ -1,5 +1,16 @@
 package com.tech.reverse;
 
 public class ReverseStringProgram {
+	public static void main(String[] args) {
 
+		String str="Balaiah";
+		char[] ch=str.toCharArray();
+		String reverse="";
+		
+		for( int i=ch.length-1;i>=0;i--) {
+			reverse=reverse+ch[i];
+		}
+		System.out.println(str);
+		System.out.println(reverse);
+	}
 }
